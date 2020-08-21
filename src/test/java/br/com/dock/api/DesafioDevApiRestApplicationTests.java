8ce786ab37e2d136f4f7870d842044e48a77ab1e
Thiago Gregorio <thiago.gregorio@hubchain.io>
2020-08-21 10:22:05 -0300

    Inclusão dos fontes iniciais do projeto

---
+++ src/test/java/br/com/dock/api/DesafioDevApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dock.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDevApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
